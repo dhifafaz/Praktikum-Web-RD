5dbe1cbd425957b1849b137bedac42c8cb769171
dhifafaz <dhifaf.az@gmail.com>
2021-11-12 13:14:05 +0700

    push

---
+++ Minggu5/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="keywords" content="Tugas Pemrograman Web" >
+        <meta name="author" content="Dhifaf Athiyah Zhabiyan - 119140047" >
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Title -->
+        <title>Tugas Praktikum 3 PEMWEB RD - Dhifafa Athiyah Z - 119140047</title>
+        
+        <!-- Styling and logo -->
+        <link rel="shortcut icon" type="image/png" href="./assets/images/Logo_ITERA.png" >
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="./assets/css/index.css">
+        <link rel="stylesheet" href="./assets/css/jquery-ui.css">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Ubuntu', sans-serif;
+                -webkit-font-smoothing: antialiased
+            }
+        </style>
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            <h1>Tugas Praktikum PEMWEB RD Minggu 5 - PHP 1</h1>
+            <div id="operasi1" >
+                <?php
+                    $a = 10;
+                    $b = 5;
+                    $c = $a + $b;
+                    echo "Hasil Penjumlahan $a + $b = $c";
+                ?>
+            </div>
+        </div>
+        
+    <!-- Script Js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="./assets/js/index.js"></script>
+    </body>
+</html>
+
